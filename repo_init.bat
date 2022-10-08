02bece427ee49c4e5f1228ce2fd8df7454f438f3
asm128 <pabloarielz@outlook.com>
2022-10-08 03:52:43 -0300

    Updated submodule heads

---
+++ repo_init.bat
@@ -0,0 +1,3 @@
+git submodule init
+git submodule update --recursive
+gpk_games\gpk_games.sln
---
+++ repo_update_heads.bat
@@ -0,0 +1,15 @@
+cd gpk
+git checkout master
+git pull
+cd ..
+cd zlib
+git checkout master
+git pull
+cd ..
+cd gpk_games
+git checkout master
+git pull
+cd ..
+git add .
+git commit -m "Updated submodule heads"
+git push
