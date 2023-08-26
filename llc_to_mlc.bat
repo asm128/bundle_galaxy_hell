57c73e63427be8bdfd5984dc884008fec258826c
asm128 <pabloarielz@outlook.com>
2023-08-26 03:08:26 -0300

    Added some scripts.

---
+++ llc_to_mlc.bat
@@ -0,0 +1,15 @@
+copy .\gpk\llc\*.h		.\mlc\lib\llc\*.h 
+copy .\gpk\llc\*.cpp	.\mlc\lib\llc\*.cpp
+
+del .\mlc\lib\llc\gpk_deflate.h 
+del .\mlc\lib\llc\gpk_deflate.cpp
+
+del .\mlc\lib\llc\gpk_png.h 
+del .\mlc\lib\llc\gpk_png*.cpp
+
+del .\mlc\lib\llc\gpk_block.h
+del .\mlc\lib\llc\gpk_block.cpp
+del .\mlc\lib\llc\gpk_mapblock.cpp
+
+
+
---
+++ mlc_to_llc.bat
@@ -0,0 +1,2 @@
+copy .\mlc\lib\llc\*.h		.\gpk\llc\*.h		
+copy .\mlc\lib\llc\*.cpp	.\gpk\llc\*.cpp		
